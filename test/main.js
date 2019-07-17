207ebd6cbeac3c82fa80bf36f422603d64a9b034
Oleg Demidov <demidov@wad-jet.ru>
2019-07-17 23:06:32 +0300

    Added main tests

---
+++ test/main.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-undef */
+var chai = require('chai');
+var expect = chai.expect;
+var di = require('../src/dependencies');
+
+// Configure chai
+chai.should();
+
+describe('Main tests', function() {
+    describe('Setup', function() {
+        it('The first argument of setup should be a function only', function() {
+            expect(() => di.setup('not a function')).to.throw('not a function');
+        });
+
+        it('The keyNameNormalization option should be undefined or a function', function() {
+            // eslint-disable-next-line no-unused-vars
+            expect(() => di.setup(c => { }, {
+                keyNameNormalization: 'not a function'
+            })).to.throw('not a function');
+        });
+
+        it('The keyNameNormalization option is undefined, used to camel case converter by default', function() {
+            expect(() => di.setup(c => { c.transient('not a function') })).to.throw('not a class or function');
+        });
+    });
+});
