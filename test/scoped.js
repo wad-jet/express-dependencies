7930bf4dd2b840f8bb62db61b0a841d64225dd02
Oleg Demidov <demidov@wad-jet.ru>
2020-02-29 12:42:18 +0300

    Unit tests for modules setup

--- test/scoped.js
+++ test/scoped.js
@@ -3,6 +3,7 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var expect = chai.expect;
 var app = require('./service/server');
+var deps = require('../src/injection');
 
 // Configure chai
 chai.use(chaiHttp);
@@ -46,5 +47,61 @@ describe('Test object registered as a scoped', function() {
                             });
                     });
             });
+    });
+
+    describe("Registering and resolving an instance", function() {
+        it('The transientScoped function test', function() {
+            const injection = deps();
+
+            const ClassConstructor = function() { this.foo = 'bar'; };
+
+            injection.setup(container => {
+                container.transientScoped(ClassConstructor);
+            });
+
+            const resolver1 = injection.getResolver('classConstructor');
+            const instance11 = resolver1();
+            const instance12 = resolver1();
+
+            const resolver2 = injection.getResolver('classConstructor');
+            const instance21 = resolver2();
+            const instance22 = resolver2();
+
+            expect(instance11).to.be.an('object'); expect(instance11).to.include({ foo: 'bar' });
+            expect(instance11).to.eq(instance12);
+
+            expect(instance21).to.be.an('object'); expect(instance21).to.include({ foo: 'bar' });
+            expect(instance21).to.eq(instance22);
+
+            expect(instance11).not.to.eq(instance22);
+            expect(instance21).not.to.eq(instance12);
+        });
+    });
+
+    describe("Registering and resolving an instance with using of a named resolver", function() {
+        it('The transientScopedNamed function test', function() {
+            const injection = deps();
+
+            const ClassConstructor = function() { this.foo = 'bar'; };
+
+            injection.setup(container => {
+                container.transientScopedNamed('MyName', ClassConstructor);
+            });
+
+            const resolver = injection.getResolver('myName');
+            const instance1 = resolver();
+            const instance2 = resolver();
+
+            expect(instance1).to.be.an('object'); expect(instance1).to.include({ foo: 'bar' });
+            expect(instance1).to.eq(instance2);
+        });
+
+        it('Invalid resolverName argument', function() {
+            const injection = deps();
+            const ClassConstructor = function() { this.foo = 'bar'; };
+            injection.setup(container => {
+                expect(() => { container.transientScopedNamed(undefined, ClassConstructor); }).to.throw('Invalid resolverName argument');
+            });
+        });
     });
 });
---
+++ test/setup.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+var chai = require('chai');
+var expect = chai.expect;
+var di = require('../src/injection')();
+var util = require('util');
+
+// Configure chai
+chai.should();
+
+describe('Setup tests', function() {
+    describe('Setup arguments validation test', function() {
+        it('The argument "setContainer" is not a function.', function() {
+            expect(() => { di.setup(undefined); }).to.throw('The argument "setContainer" is not a function.');
+        });
+
+        it('First argument of setup should be a function only', function() {
+            expect(() => di.setup('not a function')).to.throw('not a function');
+        });
+    });
+
+    describe('keyNameNormalization option test', function() {
+        it('keyNameNormalization option should be undefined or a function', function() {
+            // eslint-disable-next-line no-unused-vars
+            expect(() => di.setup(c => { }, {
+                keyNameNormalization: 'not a function'
+            })).to.throw('not a function');
+        });
+
+        it('keyNameNormalization option is undefined, used to camel case converter by default', function() {
+            expect(() => di.setup(c => { c.transient('not a function') })).to.throw('not a class or function');
+        });
+
+        it('keyNameNormalization should support camel case logic for naming by default', function() {
+            const instance = { foo: 'bar' };
+            di.setup(c => {
+                c.instance('NameForResolvinG', instance);
+            });
+
+            const resolved = { };
+            di.injector(resolved, null, () => { });
+
+            expect(resolved).to.not.have.own.property('NameForResolvinG')
+            expect(resolved).to.have.own.property('nameForResolvinG');
+            expect(resolved.nameForResolvinG()).eq(instance);
+        });
+
+        it('if key is null, undefined or empty after keyNameNormalization execute', function() {
+            expect(() => di.setup(c => { c.instance('', {}); })).to.throw("Argument \"key\" after keyNameNormalization execute has null, undefined or empty value");
+        });
+    })
+});
